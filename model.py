8b4829f32767a27efca1bfa48b33431f471f6d26
caltong <caltong1027@gmail.com>
2019-04-02 14:15:48 +0800

    CNN

--- model.py
+++ model.py
@@ -32,10 +32,9 @@ def load_train_data(path):
 
 
 train_data, train_label = load_train_data('train.csv')
-train_data = train_data.T
-train_label = train_label.reshape(1, -1)
+train_data = train_data.reshape(train_data.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 print(train_data.shape, train_label.shape)
-print(train_label[0][1])
 
 x_train = train_data[:40000]
 y_train = train_label[:40000]
@@ -44,5 +43,19 @@ y_test = train_label[40000:]
 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
 model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy', score[1])
