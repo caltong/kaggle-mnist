30d5f4ab66c113eb2a2637df56eac9b47abfe33b
caltong <caltong1027@gmail.com>
2019-04-04 14:31:15 +0800

    修改模型达到0.99928正确率

--- model_use_mnist.py
+++ model_use_mnist.py
@@ -7,7 +7,7 @@ import struct
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPool2D, BatchNormalization
 
 batch_size = 128
 num_classes = 10
@@ -96,16 +96,33 @@ print('x_train.shape' + str(x_train.shape))
 print('y_train.shape' + str(y_train.shape))
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu', input_shape=input_shape))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(512, activation="relu"))
+model.add(Dense(256, activation="relu"))
+model.add(BatchNormalization())
 model.add(Dropout(0.25))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(10, activation="softmax"))
 
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
